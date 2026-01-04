d1546fbd8a8764594bdb9eaaae901826e83016b1
rootuc <rootuc@notunixchan>
2026-01-04 16:55:59 +0800

    update

--- up-github-vec2.sh
+++ up-github-vec2.sh
@@ -3,7 +3,9 @@ git remote add gh_vec2 git@github.com:vector0902/ubuntu-test.git
 
 git add .
 git commit -am update
-git push -f gh_vec2 || sudo su vec2 -c 'git push -f gh_vec2'
+# git push -f gh_vec2 || sudo su vec2 -c 'git push -f gh_vec2'
+git push -f gh_vec2 || \
+    proxychains git push -f gh_vec2
 
 # to pull:
 # git pull gh_vec2 main
